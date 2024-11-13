9dc440025a421f4ae17aafd76a20ae83ad3b3793
SofiaPalacio <sofia.spm.palacio@gmail.com>
2024-11-13 17:16:52 -0500

    Nuevo texto

--- Formulario/app/src/main/java/com/example/formulario/MainActivity.kt
+++ Formulario/app/src/main/java/com/example/formulario/MainActivity.kt
@@ -4,5 +4,5 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import androidx.core.view.WindowInsetsCompatLNVLXKNVLXKVNXLKVN
+JSDHFILSJSHDDDHDGJSHGJSHGFJSHGDFBJHSBF
